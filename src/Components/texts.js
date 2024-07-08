a19d8ef00535bf3fa2a649f0ffd7889811a9d2b8
Hernan Rotellini <hernanrotellini@hotmail.com>
2024-07-08 11:11:40 -0300

    mod2

--- src/Components/texts.js
+++ src/Components/texts.js
@@ -1,7 +1,7 @@
 export const home = {
   title: "¿Quienes somos?",
-  image: "/img/phone-icon.png",
-  responsiveImage: "/img/phone-icon-2.png",
+  image: "/phone-icon.png",
+  responsiveImage: "/phone-icon-2.png",
   description1: "Somos una empresa con experiencia profesional en el sector público y privado, con los más altos estándares de asesoría en la industria aseguradora.",
   description2: "Nuestra diferencia está basada en los procesos de asesoría previa y en la recopilación de datos, que nos permita diseñar una solución adecuada para las necesidades de nuestros clientes.",
   description3: "Nuestra base de asesoría es ayudar a que puedan hacer una correcta transferencia de riesgos de sus proyectos personales y empresariales, para proteger su patrimonio.",
