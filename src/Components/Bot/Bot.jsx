9cb3c271b936f8f905e4a65feda7f7505f45fb06
Hernan Rotellini <hernanrotellini@hotmail.com>
2024-08-07 12:39:35 -0300

    Mod 19 Whatsapp

--- src/Components/Bot/Bot.jsx
+++ src/Components/Bot/Bot.jsx
@@ -3,7 +3,7 @@ const Bot = () => {
   const path = window.location.pathname;
   const key = path;
     return key === '/'+process.env.REACT_APP_EMAIL_TOKEN ? (
-      <div className="fixed bottom-5 sm:right-8 right-4 z-[999] w-20 h-20 cursor-pointer text-white text-4xl bg-[#F90607] w-14 h-14 flex items-center justify-center rounded-full animate-bounce">
+      <div className="fixed bottom-5 sm:right-8 right-4 z-[999] w-18 h-18 cursor-pointer text-white text-4xl bg-[#F90607] w-14 h-14 flex items-center justify-center rounded-full animate-bounce">
         <a href="https://wa.me/542616617951" target="_blank" className="flex items-center justify-center">
         <img 
         src={whatsappLogo} 
