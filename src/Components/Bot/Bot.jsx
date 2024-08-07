e319cbd27e9f1198aea76e2d92ba198e84a67c0c
Hernan Rotellini <hernanrotellini@hotmail.com>
2024-08-07 12:38:29 -0300

    Mod 19 Whatsapp

--- src/Components/Bot/Bot.jsx
+++ src/Components/Bot/Bot.jsx
@@ -1,56 +1,18 @@
 import whatsappLogo from '../../assets/whatsapp_logo.png'; 
-
-const Bot = () => { 
+const Bot = () => {
   const path = window.location.pathname;
- 
   const key = path;
-  
-  return key === "/"+process.env.REACT_APP_EMAIL_TOKEN ? (
-    <a href='https://wa.me/2616617951' target="_blank" rel="noopener noreferrer" style={styles.floatingButton}>
-      <img 
+    return key === '/'+process.env.REACT_APP_EMAIL_TOKEN ? (
+      <div className="fixed bottom-5 sm:right-8 right-4 z-[999] w-20 h-20 cursor-pointer text-white text-4xl bg-[#F90607] w-14 h-14 flex items-center justify-center rounded-full animate-bounce">
+        <a href="https://wa.me/542616617951" target="_blank" className="flex items-center justify-center">
+        <img 
         src={whatsappLogo} 
         alt="WhatsApp" 
-        style={styles.icon}
+        className="rounded-full"
       />
-    </a>
-  ) : null;
-};
-
-const styles = {
-  floatingButton: {
-    position: 'fixed',
-    bottom: '50px',
-    right: '80px',
-    backgroundColor: '#25D366',
-    color: 'white',
-    borderRadius: '50%',
-    width: '70px',
-    height: '70px',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    boxShadow: '0 2px 10px rgba(0,0,0,0.2)',
-    textDecoration: 'none',
-    overflow: 'hidden', // Asegura que la imagen no se desborde del botón
-    animation: 'bounce 1s infinite' // Aplica la animación
-  },
-  icon: {
-    width: '100%',
-    height: '100%',
-    objectFit: 'cover' // Asegura que la imagen cubra completamente el botón
-  },
-  '@keyframes bounce': {
-    '0%, 20%, 50%, 80%, 100%': {
-      transform: 'translateY(0)', 
-    },
-    '40%': {
-      transform: 'translateY(-15px)', 
-    },
-    '60%': {
-      transform: 'translateY(-10px)', 
-    }
-  }
-};
-
-
-export default Bot;
+        </a>
+      </div>
+    ) : null;
+  };
+  
+  export default Bot;
